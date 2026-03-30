aa4534eff00ef253809c4a427d0f62c33909a485
Stgram <sahil272025@gmail.com>
2026-03-31 01:51:40 +0530

    Create MainActivity2.java

---
+++ MainActivity2.java
@@ -0,0 +1,12 @@
+package com.spm.messenger;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
